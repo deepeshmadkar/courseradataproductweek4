96cfd3c4333c542bb9f962748b9ac31f1e8d2c78
deepeshmadkar <deepesh@zivtrix.in>
2018-10-30 15:35:02 +0530

    uploaded the file to shiny.io and added comment to the code

--- irisApp/server.R
+++ irisApp/server.R
@@ -1,29 +1,29 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# loading the required library
 library(shiny)
+# the e1071 is specifically loaded for caret package
+# without that library things are running properly in local environment.
+# but when uploaded to the server shinyapps.io we need to load this e1071, 
+# else server keeps on disconnected.
+library(e1071)
 library(caret)
 
-# Define server logic required to draw a histogram
+
+# Define server logic 
 shinyServer(function(input, output) {
   set.seed(101)
   
+  # Training the model
   # Linear Discriminant Analysis (LDA)
   model1 <- train(Species~., data=iris, method='lda')
   
-  
+  # Adding the reactive component
   model1pred <- reactive({
     sepalLength <- input$slength
     sepalWidth <- input$swidth
     petalLength <- input$plength
     petalWidth <- input$pwidth
     
+    # predicting
     predict(model1, newdata = data.frame(
       Sepal.Length = sepalLength,
       Sepal.Width = sepalWidth,
@@ -33,11 +33,17 @@ shinyServer(function(input, output) {
   })
   
   
-  
+  # defining the value for display
   speciesToDisplay <- c("Setosa","Versicolor","Virginica")
   
+  # sending the prediction ouput
   output$pred1 <- renderText({
-    speciesToDisplay[model1pred()]
+    
+      # if needed we can directly add model1pred()
+      # But the result would be numerical therefore we are using
+      # speciesToDisplay vector so that it can display character
+    
+      speciesToDisplay[model1pred()]
   })
   
   
